11d907c22d345568f9de9dcf6d321ce0e7b91f98
SEAMEO-RECSAM-Makerspace <148513699+SEAMEO-RECSAM-Makerspace@users.noreply.github.com>
2023-10-20 10:42:23 +0800

    Initial upload of website

---
+++ script.js
@@ -0,0 +1,34 @@
+// Modal Image Gallery
+function onClick(element, event) {
+  document.getElementById("modal01").style.display = "block";
+  // var captionText = document.getElementById("caption");
+  // captionText.innerHTML = element.alt;
+  fetch(`include/projects/${event}.html`)
+      .then(response => {
+          if (!response.ok) {
+              throw new Error(`Looks like there was a problem. Status Code: ${response.status}`);
+          }
+          return response.text();
+      })
+      .then(data => {
+          document.getElementById('caption').innerHTML = data;
+          closeNav();
+      })
+      .catch(error => {
+          console.log('Looks like we hit a snag:', error);
+      });
+}
+
+  
+// Toggle between showing and hiding the sidebar when clicking the menu icon
+function w3_open() {
+  var mySidebar = document.getElementById("mySidebar");
+  mySidebar.style.display = 'block';
+}
+  
+// Close the sidebar with the close button
+function w3_close() {
+  var mySidebar = document.getElementById("mySidebar");
+  mySidebar.style.display = "none";
+}
+
